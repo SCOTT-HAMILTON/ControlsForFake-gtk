76992c2bb216b95643358843d4c9a0a259a13061
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2020-10-23 13:07:20 +0200

    Fix .travis.yml

--- utils/build.sh
+++ utils/build.sh
@@ -5,13 +5,15 @@ DEBIAN_FRONTEND="noninteractive" apt install ninja-build libgtk-3-dev \
 	libgtkmm-3.0-dev libpulse-dev libogg-dev libvorbis-dev python3-pip \
 	python3-setuptools $COMPILER_PKG git cmake pkg-config valac -y
 python3 -m pip install meson
-git clone https://github.com/SCOTT-HAMILTON/CFakeLib
-cd CFakeLib
+
+git clone https://github.com/SCOTT-HAMILTON/FakeLib
+cd FakeLib
 mkdir build
 meson build
 ninja -C build
 ninja -C build install
 cd ..
+
 git clone https://github.com/p-ranav/argparse
 cd argparse
 mkdir build
@@ -21,6 +23,23 @@ cd ..
 ninja -C build
 ninja -C build install
 cd ..
+
+git clone https://github.com/SCOTT-HAMILTON/FakeMicWavPlayer
+cd FakeMicWavPlayer
+mkdir build
+meson build
+ninja -C build
+ninja -C build install
+cd ..
+
+git clone https://github.com/SCOTT-HAMILTON/CFakeLib
+cd CFakeLib
+mkdir build
+meson build
+ninja -C build
+ninja -C build install
+cd ..
+
 git clone https://github.com/SCOTT-HAMILTON/CFakeMicWavPlayer
 cd CFakeMicWavPlayer
 mkdir build
